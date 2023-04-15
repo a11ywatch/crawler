e331f310c9fc287e15f76a0551896d28b64e3190
j-mendez <jeff@a11ywatch.com>
2023-04-15 13:46:02 -0400

    perf(io): stream html body

--- src/packages/spider/utils.rs
+++ src/packages/spider/utils.rs
@@ -2,16 +2,26 @@ use log::{info, log_enabled, Level};
 use reqwest::Client;
 use reqwest::StatusCode;
 
-/// Perform a network request to a resource extracting all content as text.
+/// Perform a network request to a resource extracting all content as text streaming.
 pub async fn fetch_page_html(url: &str, client: &Client) -> Option<String> {
+    use tokio_stream::StreamExt;
+
     match client.get(url).send().await {
-        Ok(res) if res.status() == StatusCode::OK => match res.text().await {
-            Ok(text) => Some(text),
-            Err(_) => {
-                log("- error fetching {}", &url);
-                None
+        Ok(res) if res.status() == StatusCode::OK => {
+            let mut stream = res.bytes_stream();
+            let mut data: String = String::new();
+
+            while let Some(item) = stream.next().await {
+                match item {
+                    Ok(text) => {
+                        data += &String::from_utf8_lossy(&text);
+                    }
+                    _ => (),
+                }
             }
-        },
+
+            Some(data)
+        }
         Ok(_) => None,
         Err(_) => {
             log("- error parsing html text {}", &url);
@@ -19,6 +29,7 @@ pub async fn fetch_page_html(url: &str, client: &Client) -> Option<String> {
         }
     }
 }
+
 
 /// log to console if configuration verbose.
 pub fn log(message: &'static str, data: impl AsRef<str>) {
